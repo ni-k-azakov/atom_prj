54b41c247cc9f735ab0dc90317c380be68758300
Ъ <55149626+ni-k-azakov@users.noreply.github.com>
2021-11-23 02:30:24 +0300

    Update: form email send

--- php/send.php
+++ php/send.php
@@ -1,23 +1,19 @@
 <?php
-$recaptcha = $_POST['g-recaptcha-response'];
-if(!empty($recaptcha)) {
-        $fio = $_POST['fio'];
-        $email = $_POST['email'];
-        $text = $_POST['QUESTION']
-        $fio = htmlspecialchars($fio);
-        $email = htmlspecialchars($email);
-        $text =  htmlspecialchars($text);
-        $fio = urldecode($fio);
-        $email = urldecode($email);
-        $text = urldecode($text);
-        $fio = trim($fio);
-        $text = trim($text);
-        $email = trim($email);
-        if (mail("balansavto@mail.ru", "Заявка с сайта", "От: ".$fio.". E-mail/телефон: ".$email., "Сообщение: ".$text."From: info@balansavto.ru \r\n"))
-         {     echo "сообщение успешно отправлено";
-        } else {
-            echo "при отправке сообщения возникли ошибки";
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$text = $_POST['message']
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$text =  htmlspecialchars($text);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$text = urldecode($text);
+$fio = trim($fio);
+$text = trim($text);
+$email = trim($email);
+if (mail("nikkazakov909@gmail.com", "Заявка с сайта", "От: ".$fio.". E-mail/телефон: ".$email., "Сообщение: ".$text."From: info@balansavto.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
 } else {
-        echo "поставьте галочку в поле 'Я не робот' для отправки сообщения";
-}
+    echo "при отправке сообщения возникли ошибки";
 }?>
+
