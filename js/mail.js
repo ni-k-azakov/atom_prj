3de84f051adf601d097734e7da604b31149dba91
Ъ <55149626+ni-k-azakov@users.noreply.github.com>
2021-12-06 19:28:07 +0300

    dfg

--- js/mail.js
+++ js/mail.js
@@ -1,37 +1,9 @@
-
-function SendMail(sRecipientMail, sSubject, sMsgBody)   
-{   
-    try   
-    {   
-        // create a session and log on -- username and password in profile    
-        var refMsg = WScript.CreateObject("CDO.Message");   
-        var refConf = WScript.CreateObject("CDO.Configuration");   
-           
-        // Setting configuration params   
-        with(refConf.Fields)   
-        {   
-            Item("http://schemas.microsoft.com/cdo/configuration/smtpserver") = "smtp.mail.ru";  
-            Item("http://schemas.microsoft.com/cdo/configuration/sendusing") = 2; 
-            Item("http://schemas.microsoft.com/cdo/configuration/smtpauthenticate") = 1;  
-            Item("http://schemas.microsoft.com/cdo/configuration/sendusername") = sRecipientMail;  
-            Item("http://schemas.microsoft.com/cdo/configuration/sendpassword") = "****"; 
-        }   
-        refConf.Fields.Update();   
-   
-        with(refMsg)   
-        {   
-            Configuration = refConf;
-            To       = "nikkazakov909@gmail.com";   
-            From     = sRecipientMail;   
-            Subject  = sSubject;   
-            TextBody = sMsgBody;
-        } 
-  
-        refMsg.Send();
-    }    
-    catch(e)   
-    {   
-        WScript.Echo("SendMail error !!! : " + e.description);   
-        WScript.Quit(1);   
-    }   
+function sendMail(from, name, message) {
+    var link = "mailto:nikkazakov909@gmail.com"
+             + "?cc=" + from
+             + "&subject=" + encodeURIComponent(name)
+             + "&body=" + encodeURIComponent(message)
+    ;
+    
+    window.location.href = link;
 }
