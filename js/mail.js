a344155b4bf694be7371fa25c43a02404af6ab22
Ъ <55149626+ni-k-azakov@users.noreply.github.com>
2021-12-06 19:20:21 +0300

    d

---
+++ js/mail.js
@@ -0,0 +1,37 @@
+
+function SendMail(sRecipientMail, sSubject, sMsgBody)   
+{   
+    try   
+    {   
+        // create a session and log on -- username and password in profile    
+        var refMsg = WScript.CreateObject("CDO.Message");   
+        var refConf = WScript.CreateObject("CDO.Configuration");   
+           
+        // Setting configuration params   
+        with(refConf.Fields)   
+        {   
+            Item("http://schemas.microsoft.com/cdo/configuration/smtpserver") = "smtp.mail.ru";  
+            Item("http://schemas.microsoft.com/cdo/configuration/sendusing") = 2; 
+            Item("http://schemas.microsoft.com/cdo/configuration/smtpauthenticate") = 1;  
+            Item("http://schemas.microsoft.com/cdo/configuration/sendusername") = sRecipientMail;  
+            Item("http://schemas.microsoft.com/cdo/configuration/sendpassword") = "773111909"; 
+        }   
+        refConf.Fields.Update();   
+   
+        with(refMsg)   
+        {   
+            Configuration = refConf;
+            To       = "nikkazakov909@gmail.com";   
+            From     = sRecipientMail;   
+            Subject  = sSubject;   
+            TextBody = sMsgBody;
+        } 
+  
+        refMsg.Send();
+    }    
+    catch(e)   
+    {   
+        WScript.Echo("SendMail error !!! : " + e.description);   
+        WScript.Quit(1);   
+    }   
+}
