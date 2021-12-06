bf8fe739085957eb07e6e482393a7480430f50db
Ъ <55149626+ni-k-azakov@users.noreply.github.com>
2021-12-06 20:19:04 +0300

    dfgdfg

--- js/car_checker.js
+++ js/car_checker.js
@@ -1,35 +1,84 @@
 function m_first() {
-                        document.getElementById('12m').className = 'active';
-                        document.getElementById('18m').className = 'inactive';
-                        document.getElementById('22m').className = 'inactive';
-                        document.getElementById('30m').className = 'inactive';
-                        document.getElementById('35m').className = 'inactive';
-                }
-                function m_second() {
-                        document.getElementById('18m').className = 'active';
-                        document.getElementById('12m').className = 'inactive';
-                        document.getElementById('22m').className = 'inactive';
-                        document.getElementById('30m').className = 'inactive';
-                        document.getElementById('35m').className = 'inactive';
-                }
-                function m_third() {
-                        document.getElementById('22m').className = 'active';
-                        document.getElementById('12m').className = 'inactive';
-                        document.getElementById('18m').className = 'inactive';
-                        document.getElementById('30m').className = 'inactive';
-                        document.getElementById('35m').className = 'inactive';
-                }
-                function m_fourth() {
-                        document.getElementById('30m').className = 'active';
-                        document.getElementById('12m').className = 'inactive';
-                        document.getElementById('18m').className = 'inactive';
-                        document.getElementById('22m').className = 'inactive';
-                        document.getElementById('35m').className = 'inactive';
-                }
-                function m_fifth() {
-                        document.getElementById('35m').className = 'active';
-                        document.getElementById('12m').className = 'inactive';
-                        document.getElementById('18m').className = 'inactive';
-                        document.getElementById('22m').className = 'inactive';
-                        document.getElementById('30m').className = 'inactive';
-                }
+        document.getElementById('12m').className = 'active';
+        document.getElementById('18m').className = 'inactive';
+        document.getElementById('22m').className = 'inactive';
+        document.getElementById('30m').className = 'inactive';
+        document.getElementById('35m').className = 'inactive';
+
+
+        document.getElementById('12_meters_h').style.color = 'black';
+        document.getElementById('18_meters_h').style.color = 'grey';
+        document.getElementById('22_meters_h').style.color = 'grey';
+        document.getElementById('30_meters_h').style.color = 'grey';
+        document.getElementById('35_meters_h').style.color = 'grey';
+
+        document.getElementById("vehicle_name").innerHTML = 'Mitsubishi';
+}
+
+function m_second() {
+        document.getElementById('18m').className = 'active';
+        document.getElementById('12m').className = 'inactive';
+        document.getElementById('22m').className = 'inactive';
+        document.getElementById('30m').className = 'inactive';
+        document.getElementById('35m').className = 'inactive';
+
+
+        document.getElementById('12_meters_h').style.color = 'grey';
+        document.getElementById('18_meters_h').style.color = 'black';
+        document.getElementById('22_meters_h').style.color = 'grey';
+        document.getElementById('30_meters_h').style.color = 'grey';
+        document.getElementById('35_meters_h').style.color = 'grey';
+
+        document.getElementById("vehicle_name").innerHTML = 'Hyundai';
+}
+
+function m_third() {
+        document.getElementById('22m').className = 'active';
+        document.getElementById('12m').className = 'inactive';
+        document.getElementById('18m').className = 'inactive';
+        document.getElementById('30m').className = 'inactive';
+        document.getElementById('35m').className = 'inactive';
+
+
+        document.getElementById('12_meters_h').style.color = 'grey';
+        document.getElementById('18_meters_h').style.color = 'grey';
+        document.getElementById('22_meters_h').style.color = 'black';
+        document.getElementById('30_meters_h').style.color = 'grey';
+        document.getElementById('35_meters_h').style.color = 'grey';
+
+        document.getElementById("vehicle_name").innerHTML = 'Hyundai';
+}
+
+function m_fourth() {
+        document.getElementById('30m').className = 'active';
+        document.getElementById('12m').className = 'inactive';
+        document.getElementById('18m').className = 'inactive';
+        document.getElementById('22m').className = 'inactive';
+        document.getElementById('35m').className = 'inactive';
+
+
+        document.getElementById('12_meters_h').style.color = 'grey';
+        document.getElementById('18_meters_h').style.color = 'grey';
+        document.getElementById('22_meters_h').style.color = 'grey';
+        document.getElementById('30_meters_h').style.color = 'black';
+        document.getElementById('35_meters_h').style.color = 'grey';
+
+        document.getElementById("vehicle_name").innerHTML = 'Hyundai';
+}
+
+function m_fifth() {
+        document.getElementById('35m').className = 'active';
+        document.getElementById('12m').className = 'inactive';
+        document.getElementById('18m').className = 'inactive';
+        document.getElementById('22m').className = 'inactive';
+        document.getElementById('30m').className = 'inactive';
+
+
+        document.getElementById('12_meters_h').style.color = 'grey';
+        document.getElementById('18_meters_h').style.color = 'grey';
+        document.getElementById('22_meters_h').style.color = 'grey';
+        document.getElementById('30_meters_h').style.color = 'grey';
+        document.getElementById('35_meters_h').style.color = 'black';
+
+        document.getElementById("vehicle_name").innerHTML = 'Hyundai';
+}
